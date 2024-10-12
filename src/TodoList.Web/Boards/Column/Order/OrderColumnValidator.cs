4a6c1d70b529bd69bf1617e71b98c4f134409064
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-10-12 16:34:00 +0300

    Add order column validator

---
+++ src/TodoList.Web/Boards/Column/Order/OrderColumnValidator.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace TodoList.Web.Boards.Column.Order;
+
+public class OrderColumnValidator : Validator<OrderColumnRequest>
+{
+    public OrderColumnValidator()
+    {
+        RuleFor(x => x.ColumnId)
+            .NotEmpty().WithMessage("Board ID is required.");
+
+        RuleFor(x => x.Position);
+    }
+}
